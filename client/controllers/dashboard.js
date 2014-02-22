535314b2627abdc2f90909fdf4d383da37352c54
gdi2290 <github@gdi2290.com>
2014-02-22 15:29:53 -0800

    update dashboard

--- client/controllers/dashboard.js
+++ client/controllers/dashboard.js
@@ -1,6 +1,30 @@
 'use strict';
 
 angular.module('app.controllers')
-.controller('DashboardCtrl', function($scope) {
+.controller('DashboardCtrl', function($scope, $http) {
   $scope.world = 'World';
+
+  $scope.realtime = true;
+
+  $scope.sessions = [
+    {
+      id: 677,
+      byDate : new Date(),
+      length: 234,
+      seconds: 17
+    },
+    {
+      id: 677,
+      byDate : new Date(),
+      length: 234,
+      seconds: 17
+    },
+    {
+      id: 677,
+      byDate : new Date(),
+      length: 234,
+      seconds: 17
+    }
+  ];
+
 });
