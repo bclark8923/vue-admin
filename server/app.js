bec3bfce643bc4a830c39df34cc026e3dd2619d2
gdi2290 <github@gdi2290.com>
2014-02-22 12:35:17 -0800

    add server.js and config.json

---
+++ server/app.js
@@ -0,0 +1,50 @@
+var NODE_ENV = process.env.NODE_ENV = (process.env.NODE_ENV || 'production');
+var PORT = process.env.PORT = (process.env.PORT || 3000);
+var express = require('express');
+var stylus = require('stylus');
+var path = require('path');
+var cors = require('cors');
+var config = require('./config.json')[NODE_ENV];
+
+
+module.exports = function(SERVER_ROOT) {
+  var app = express();
+  app.directory = SERVER_ROOT;
+
+  // Store all environment variables
+  app.set('port', PORT);
+  app.set('env', NODE_ENV);
+
+  app.configure(function() {
+    // App middleware
+    app.use(cors());
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(app.directory, 'public'));
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    // app.use(express.session());
+    app.use('/bower_components', express.static(path.join(app.directory, 'bower_components')));
+    app.use('/templates', express.static(path.join(app.directory, 'client/templates')));
+    app.use(express.static(path.join(app.directory, 'public')));
+    app.use(app.router);
+    app.use(stylus.middleware({
+      src: app.directory + '/client/styles',
+      dest: app.directory + '/public/stylesheets',
+      compile: function(str, path, fn) {
+        stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .render(fn);
+      }
+    }));
+  });
+
+
+  app.get('/*', function(req, res) {
+    res.render('index', {env: app.get('env')});
+  });
+
+
+  return app;
+};
