650a8177c748cf5d4ed5f9266806d66671f7c491
gdi2290 <github@gdi2290.com>
2014-02-22 15:29:17 -0800

    init questions service

---
+++ client/services/questions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Services */
+angular.module('app.services')
+.factory('Questions', function($http) {
+  var _questions = [];
+
+  return {
+    get: function() {
+
+    },
+  };
+
+});
