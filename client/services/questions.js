c3d92788b371fd50b40c70a38298d21fef41f2b6
gdi2290 <github@gdi2290.com>
2014-02-22 17:34:45 -0800

    refactor questions with pages

--- client/services/questions.js
+++ client/services/questions.js
@@ -4,26 +4,31 @@
 angular.module('app.services')
 .factory('Questions', function($http, $q, _) {
   var _questions = [];
-  var cache = {};
+
+  var _page = [];
+
   return {
     sync: function() {
+      var self = this;
       console.log('Questions: SYNC', _questions);
       var dfd = $q.defer();
 
       $http.get('/api/sessions')
       .success(function(sessions, length) {
         console.log('Questions: success', sessions, length);
-        cache[0] = _.sample(sessions, 20);
-        cache[1] = _.sample(sessions, 20);
-        cache[2] = _.sample(sessions, 20);
-        _questions = cache[0];
+        _page.push(_.sample(sessions, 20));
+        _page.push(_.sample(sessions, 20));
+        _page.push(_.sample(sessions, 20));
+        _questions = _page[self.page];
         dfd.resolve(_questions);
       });
 
+
       return dfd.promise;
     },
+    page: 0,
     pagination: function(page) {
-      return cache[page];
+      return _page[page];
     },
     get: function() {
       console.log('Questions: GET', _questions);
