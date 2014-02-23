9c06b64499fe26cd23f5da6b9c424104dc779801
gdi2290 <github@gdi2290.com>
2014-02-23 10:12:52 -0800

    create interactionsTree and include underscore

--- database/index.js
+++ database/index.js
@@ -1,14 +1,24 @@
 'use strict';
-
+var _ = require('underscore');
 var database = {
   sessions: require('./sessions.json')
 };
+_.each(database.sessions, function(session) {
+  session.interactionsTree = session.interactionsTree || [];
+  var previous = null;
+  session.interactions.forEach(function(interaction) {
+    if (interaction.page !== previous) {
+      session.interactionsTree.push(interaction.page);
+    }
+    previous = interaction.page;
+  });
+});
 
 module.exports = function(app, config) {
   console.log(config);
   var databaseUrl = config.mongoDB;
-  var collections = ["questions"]
-  var db = require("mongojs").connect(databaseUrl, collections);
+  var collections = ['questions'];
+  var db = require('mongojs').connect(databaseUrl, collections);
 
   var DB = {
     get: function(data) {
