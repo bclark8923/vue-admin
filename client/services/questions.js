45dc247bbe1c7112b866dde4f067391ff76364bb
gdi2290 <github@gdi2290.com>
2014-02-22 16:01:41 -0800

    fix scroll issue

--- client/services/questions.js
+++ client/services/questions.js
@@ -2,13 +2,36 @@
 
 /* Services */
 angular.module('app.services')
-.factory('Questions', function($http) {
+.factory('Questions', function($http, $q, _) {
   var _questions = [];
 
   return {
-    get: function() {
+    sync: function() {
+      console.log('Questions: SYNC', _questions);
+      var dfd = $q.defer();
 
+      $http.get('/api/sessions')
+      .success(function(sessions, length) {
+        console.log('Questions: success', sessions, length);
+        _questions = sessions;
+        dfd.resolve(_questions);
+      });
+
+      return dfd.promise;
     },
+    get: function() {
+      console.log('Questions: GET', _questions);
+      return _questions;
+    },
+    first: function() {
+      return _.first(_questions, 20);
+    },
+    range: function() {
+      // return _.range();
+    },
+    query: function() {
+
+    }
   };
 
 });
